27fe5ab0759c3f9b955e36cc3065365d2312d894
AnikethSDeshpande <anikethsdeshpande@gmail.com>
2019-12-26 21:49:03 +0530

    faculty_update_insert_qb API tested and verified

---
+++ admin/faculty_qb.py
@@ -0,0 +1,53 @@
+# Author: Aniketh S Deshpande
+# API-name: Login
+# Flask-Server
+# Database: MongoDB
+
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from config import get_ip
+
+ip_address = get_ip()
+
+app = Flask(__name__)
+CORS(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/quiz"
+api = Api(app)
+mongo = PyMongo(app)
+
+
+
+class FacultyContributeQB(Resource):
+    def post(self):
+        obj = request.get_json(force=True)
+        qb_name = obj['qb_name']
+        questions = obj['questions']
+
+        mongo_obj = dict(mongo.db.qbanks.update(
+            {
+                "name": qb_name
+            },
+
+            {
+                "$set": {
+                    "questions": questions
+                }
+            }
+        ))
+
+        if mongo_obj['nModified'] == 1:
+            return {
+                'status': 'success'
+            }
+    
+
+
+# resources routing
+api.add_resource(FacultyContributeQB, '/insert_update_qb')  
+
+
+# ipaddress loaded dynamically
+if __name__ == '__main__':
+    app.run(debug=True, host=ip_address, port=5059)
